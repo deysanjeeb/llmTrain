df752cd8cb3db4bf95d2de3173af0e70b9262f2b
Sanjeeb Dey <sanjeebdey1@gmail.com>
2024-04-26 13:32:01 -0700

    Added LLM

--- rag.py
+++ rag.py
@@ -1,15 +1,5 @@
-corpus_of_documents = [
-    "Take a leisurely walk in the park and enjoy the fresh air.",
-    "Visit a local museum and discover something new.",
-    "Attend a live music concert and feel the rhythm.",
-    "Go for a hike and admire the natural scenery.",
-    "Have a picnic with friends and share some laughs.",
-    "Explore a new cuisine by dining at an ethnic restaurant.",
-    "Take a yoga class and stretch your body and mind.",
-    "Join a local sports league and enjoy some friendly competition.",
-    "Attend a workshop or lecture on a topic you're interested in.",
-    "Visit an amusement park and ride the roller coasters."
-]
+import requests
+import json
 
 def jaccard_similarity(query, document):
     query = query.lower().split(" ")
@@ -27,9 +17,53 @@ def return_response(query, corpus):
     return corpus_of_documents[similarities.index(max(similarities))]
 
 
+corpus_of_documents = [
+    "Take a leisurely walk in the park and enjoy the fresh air.",
+    "Visit a local museum and discover something new.",
+    "Attend a live music concert and feel the rhythm.",
+    "Go for a hike and admire the natural scenery.",
+    "Have a picnic with friends and share some laughs.",
+    "Explore a new cuisine by dining at an ethnic restaurant.",
+    "Take a yoga class and stretch your body and mind.",
+    "Join a local sports league and enjoy some friendly competition.",
+    "Attend a workshop or lecture on a topic you're interested in.",
+    "Visit an amusement park and ride the roller coasters."
+]
+
 user_prompt = "What is a leisure activity that you like?"
 print(user_prompt)
 
 user_input = input()
 
-print(return_response(user_input, corpus_of_documents))
+relevant_document = return_response(user_input, corpus_of_documents)
+full_response = []
+# https://github.com/jmorganca/ollama/blob/main/docs/api.md
+
+prompt = """
+You are a bot that makes recommendations for activities. You answer in very short sentences and do not include extra information.
+This is the recommended activity: {relevant_document}
+The user input is: {user_input}
+Compile a recommendation to the user based on the recommended activity and the user input.
+"""
+
+# LLM stuff
+url = 'http://localhost:11434/api/generate'
+
+data = {
+    "model": "mistral",
+    "prompt": prompt.format(user_input=user_input, relevant_document=relevant_document)
+}
+
+headers = {'Content-Type': 'application/json'}
+response = requests.post(url, data=json.dumps(data), headers=headers, stream=True)
+try:
+    count = 0
+    for line in response.iter_lines():
+        if line:
+            decoded_line = json.loads(line.decode('utf-8'))
+            
+            full_response.append(decoded_line['response'])
+finally:
+    response.close()
+print(''.join(full_response))
+
